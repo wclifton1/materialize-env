fa24da7dae5f9c4b41db86eec1b66885442fa574
ScienceDuck <sarah.hein.0@gmail.com>
2015-01-27 21:56:36 -0600

    update

--- js/reveal.js
+++ js/reveal.js
@@ -1,5 +1,20 @@
-$(document).ready(function() {
-    $(".reveal").click(function() {
-        $(".hidden").removeClass()
+$(document).ready(function () {
+    $(".reveal").hover(function() {
+        $(this).toggleClass("light-blue lighten-3");},
+        function(){
+        $(this).toggleClass("light-blue lighten-3");
+    })
+    
+    var click = false;
+    $(".reveal").click(function () {
+        var next = $(this).next();
+        var same = $(this)
+        if(click===false) {
+            $(next).slideDown("fast");
+            click = true;
+        } else{
+            $(next).slideUp("fast");
+            click = false;
+        }
     }) 
 })
