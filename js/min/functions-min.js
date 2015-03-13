535d3bbc8e3b37486aae7754108c3f061795085c
wclifton1 <wclifton1@gmail.com>
2015-03-13 16:49:59 -0700

    bioventures and card-reveal updates

--- js/min/functions-min.js
+++
@@ -1,1 +0,0 @@
-!function($){"use strict";var t=$("html, body"),a=$("#main").smoothState({prefetch:!0,pageCacheSize:4,onStart:{duration:250,render:function(e,o){a.toggleAnimationClass("is-exiting"),t.animate({scrollTop:0})}}}).data("smoothState")}(jQuery);
