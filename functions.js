694bb5f18df6bfb35eca763dfc5cc713ffdccf2f
wclifton1 <wclifton1@gmail.com>
2015-03-15 12:53:50 -0700

    animations and jquery.smoothstate.js

---
+++ functions.js
@@ -0,0 +1,23 @@
+// Contents of functions.js
+;(function($) {
+  'use strict';
+  var $body = $('html, body'),
+      content = $('#main').smoothState({
+        prefetch: true,
+        pageCacheSize: 4,
+        // Runs when a link has been activated
+        onStart: {
+          duration: 250, // Duration of our animation
+          render: function () {
+            // toggleAnimationClass() is a public method
+            // for restarting css animations with a class
+            content.toggleAnimationClass('is-exiting');
+            // Scroll user to the top
+            $body.animate({
+              scrollTop: 0
+            });
+          }
+        }
+      }).data('smoothState');
+      //.data('smoothState') makes public methods available
+})(jQuery);
